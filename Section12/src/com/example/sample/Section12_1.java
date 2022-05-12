eac40a11977ccd35a8b9bb664aa185f3bc16ad1d
kgk565 <silsilsilvias15@gmail.com>
2022-05-12 19:24:50 +0900

    2022年5月12日

--- Section12/src/com/example/sample/Section12_1.java
+++ Section12/src/com/example/sample/Section12_1.java
@@ -1,2 +1,30 @@
-package com.example.sample;public class Section12_1 {
+package com.example.sample;
+
+import java.sql.*;
+
+public class Section12_1 {
+    public static void main(String[] args) {
+        try{
+            Class.forName("org.h2.Driver");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection("jdbc:h2:~/rpgdb");
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            if(con != null){
+                try {
+                    con.close();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
